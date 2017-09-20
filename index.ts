dc63c4ace158afd338795845396067356149de46
gengjiawen <technicalcute@gmail.com>
2017-09-20 10:41:16 +0800

    add pass header

--- index.ts
+++ index.ts
@@ -1,68 +1,61 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { makeRemoteExecutableSchema, mergeSchemas } from 'graphql-tools';
-import { createApolloFetch } from 'apollo-fetch';
+import { createProxySchema, HttpGraphQLClient } from 'graphql-weaver';
+import * as graphqlHTTP from 'express-graphql';
+import { GraphQLSchema, DocumentNode } from 'graphql';
+
+function getAuthTokenFromGraphQLContext(context: any) {
+  if (!context) {
+    console.log("not valid", context)
+    return undefined
+  }
+  // console.log('context', context);
+  console.log('headers', context.headers);
+  return context.headers['Authentication'];
+}
+
+class AuthForwardingGraphQLClient extends HttpGraphQLClient {
+  protected async getHeaders(document: DocumentNode, variables?: { [name: string]: any }, context?: any, introspect?: boolean): Promise<{ [index: string]: string }> {
+    console.log('pass', context)
+    const headers = await super.getHeaders(document, context, introspect);
+    return {
+        ...headers,
+        Authentication: getAuthTokenFromGraphQLContext(context)
+    };
+  }
+}
 
 async function run() {
-  const universeSchema = await makeRemoteExecutableSchema(createApolloFetch({
-    uri: 'https://www.universe.com/graphql/beta',
-  }));
-
-  const weatherSchema = await makeRemoteExecutableSchema(createApolloFetch({
-    uri: 'https://5rrx10z19.lp.gql.zone/graphql',
-  }));
-
-  const schema = mergeSchemas({
-    schemas: [universeSchema, weatherSchema],
-    links: [
-      {
-        name: 'location',
-        from: 'Event',
-        to: 'location',
-        resolveArgs: parent => ({ place: parent.cityName }),
-        fragment: `
-          fragment WeatherLocationArgs on Event {
-            cityName
-          }
-        `,
-      },
-    ],
-  });
+  const schema: GraphQLSchema = await createProxySchema({
+    endpoints: [{
+        client: new AuthForwardingGraphQLClient({url: 'https://www.universe.com/graphql/beta'})
+    }, {
+        namespace: 'library',
+        typePrefix: 'Zone',
+        client: new AuthForwardingGraphQLClient({url: 'https://5rrx10z19.lp.gql.zone/graphql'})
+    }]
+  })
 
   const app = express();
 
-  app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-  app.use(
-    '/graphiql',
-    graphiqlExpress({
-      endpointURL: '/graphql',
-      query: `query {
-  event(id: "5983706debf3140039d1e8b4") {
-    title
-    description
-    url
-    location {
-      city
-      country
-      weather {
-        summary
-        temperature
-      }
+  app.use('/graphql', graphqlHTTP(request => {
+    console.log(request.headers);
+    return {
+      schema: schema,
+      context: request,
+      graphiql: true
     }
-  }
-}
-      `,
-    })
-  );
+  }));
 
-  app.listen(3000);
-  console.log('Server running. Open http://localhost:3000/graphiql to run queries.');
+
+  app.listen(3210);
+  console.log('Server running. Open http://localhost:3210/graphql to run queries.');
 }
 
-try {
-  run();
-} catch (e) {
-  console.log(e, e.message, e.stack);
-}
+run()
+.then(a => {
+  console.log(a)
+})
+.catch(e => {
+  console.log(e)
+})
+;
